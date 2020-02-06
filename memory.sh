e4106e45fd546ca48d530867cc873667f0b7f101
rpj <wasmus@zom.bi>
2020-02-06 01:25:00 +0100

    memory print info

--- memory.sh
+++ memory.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
-#
-#
-#
-#
+
+print_info () {
+    echo "usage: memory deck add name"
+    echo "       memory deck del name"
+    echo "       memory deck sel name"
+    echo "       memory deck list"
+    echo "       memory card add front back"
+    echo "       memory card del id"
+    echo "       memory card list"
+}
 
 FLASHCARD_HOME="$HOME/.memory"
 DECKS="$FLASHCARD_HOME/decks"
+DEFAULT="$DECKS/default"
+
+mkdir $DEFAULT -p
+
+if [ -z $2 ]; then print_info; exit; fi
 
 if [[ $1 = "card" ]]
 then
     if [[ $2 = "add" ]]
     then
-
+        echo "add card"
     fi
 
     if [[ $2 = "del" ]]
     then
     
+        echo "add card"
     fi
 
     if [[ $2 = "list" ]]
     then
     
+        echo "add card"
     fi
 fi
 
@@ -29,16 +42,16 @@ if [[ $1 = "deck" ]]
 then
     if [[ $2 = "add" ]]
     then
-    
+        echo "add card"
     fi
 
     if [[ $2 = "del" ]]
     then
-    
+        echo "add card"
     fi
 
     if [[ $2 = "list" ]]
     then
-    
+        echo "add card"
     fi
 fi
