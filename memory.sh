055034077046f84e67ec002619590b7e717d5493
rpj <wasmus@zom.bi>
2020-02-06 01:10:30 +0100

    add memory.sh

---
+++ memory.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#
+#
+#
+
+FLASHCARD_HOME="$HOME/.memory"
+DECKS="$FLASHCARD_HOME/decks"
+
+if [[ $1 = "card" ]]
+then
+    if [[ $2 = "add" ]]
+    then
+
+    fi
+
+    if [[ $2 = "del" ]]
+    then
+    
+    fi
+
+    if [[ $2 = "list" ]]
+    then
+    
+    fi
+fi
+
+if [[ $1 = "deck" ]]
+then
+    if [[ $2 = "add" ]]
+    then
+    
+    fi
+
+    if [[ $2 = "del" ]]
+    then
+    
+    fi
+
+    if [[ $2 = "list" ]]
+    then
+    
+    fi
+fi
