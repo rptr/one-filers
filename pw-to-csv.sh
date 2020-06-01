02b2cd6f5eb8b63512ad385ada761f32e41b88a1
Rasmus Jonsson <wasmus@zom.bi>
2020-06-01 14:46:51 +0200

    change name to worse.sh

--- pw-to-csv.sh
+++ pw-to-csv.sh
@@ -8,10 +8,37 @@
 # password
 # extra details
 
-if [[ ! -f $1 ]]
+# file to read from
+filename=$1
+# file to write to
+target=$2
+# counts which line of a record we are processing (0=website, 1=username)
+line_num=0
+# parts of a particular record (website, account, password) etc 
+parts=()
+
+if [[ ! -f $filename ]]
 then
-    echo "$1 does not exist"
+    echo "file $filename does not exist"
     exit 1
 fi
+
+if [[ -f $target ]]
+then
+    echo "file $target already exists. overwrite (y/n)?"
+    exit 2
+fi
+
+while IFS= read -r line
+do
+    if [[ ${#line} = 0 ]]
+    then
+        line_num=0
+    else
+        line_num=$((line_num+1))
+    fi
+done < "$filename"
+
+
 
 
