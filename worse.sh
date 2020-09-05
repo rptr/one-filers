7b995b5158c218b1748057c9fec9ffe2954cb488
Rasmus Jonsson <wasmus@zom.bi>
2020-09-06 00:13:41 +0200

    workhorse: load projects; list projects

--- worse.sh
+++ worse.sh
@@ -14,12 +14,34 @@ function show_help {
     exit 0
 }
 
-if [[ -z "$1" || -z "$2" ]]
+function print_project {
+    IFS=':'; read -r -a parts <<< "$line"
+    echo "${parts[0]}: ${parts[1]}H ${parts[2]}M"
+}
+
+# display a list of all projects and their hours
+if [[ "$1" = "list" ]]
 then
-    show_help
+    while IFS= read -r line
+    do
+        if [[ $found_data = 1 ]]; then
+            print_project
+        fi
+
+        if [[ "$line" = "%% data" ]]; then
+            found_data=1
+        fi
+    done < "$worse_file"
+
+    exit 0
 fi
 
-if [[ "$1" = "help" ]]
+# if [[ "$1" = "help" ]]
+# then
+#     show_help
+# fi
+
+if [[ -z "$1" || -z "$2" ]]
 then
     show_help
 fi
@@ -28,22 +50,44 @@ touch $worse_file
 
 if [ ! -f $worse_file ]
 then
-    echo "please create config file: $worse_file"
+    echo "can't create config file: $worse_file"
     exit 1
 fi
 
+
+
+
+#
+#
+# Read .workhorse file to prepare for adding new hours
+#
+#
+found_data=0
+project_names=()
+project_hours=()
+project_minutes=()
+
+# process one line from .workhorse
+# e.g. "build my shed:1:20"
+# means you spent 1h20m building your shed
 function process_line {
     IFS=':'; read -r -a parts <<< "$line"
     c=0
 
     for i in "${parts[@]}"; do
-        echo $i $c
+        if [[ $c = 0 ]]; then
+            project_names+=($i)
+        elif [[ $c = 1 ]]; then
+            project_hours+=($i)
+        else 
+            project_minutes+=($i)
+        fi
+
         c=$((c + 1 % 3))
     done
 }
 
 found_data=0
-
 while IFS= read -r line
 do
     if [ $found_data = 1 ]; then
@@ -55,11 +99,12 @@ do
     fi
 done < "$worse_file"
 
-if [ $append -eq 0 ]; then
-    output="$(date)\n$text\n\n"
-else
-    output="$text\n\n"
-fi
+for ((i = 0 ; i < ${#project_names[@]} ; i++)); do
+    name=${project_names[i]}
+    hours=${project_hours[i]}
+    minutes=${project_minutes[i]}
+    printf "%s\t\t\t\t%dH %dM\n" $name $hours $minutes
+done
 
 # printf "$output" | tee -a "$diary_file"
 printf "$output"
