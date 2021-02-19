7f33e117dc0e1aed90ea4ad01d54d17820a1c56e
Rasmus Jonsson <wasmus@zom.bi>
2021-02-19 20:00:44 +0100

    scheme.hs: parser done

--- scheme.hs
+++ scheme.hs
@@ -1,11 +1,73 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
+data AST = Atom String
+               | List [AST]
+               | DottedList [AST] AST
+               | Number Integer
+               | String String
+               | Bool Bool
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser AST
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser AST
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+               "#t" -> Bool True
+               "#f" -> Bool False
+               _    -> Atom atom
+
+parseNumber :: Parser AST
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseList :: Parser AST
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser AST
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser AST
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+parseExpr :: Parser AST
+parseExpr = parseAtom
+          <|> parseString
+          <|> parseNumber
+          <|> parseQuoted
+          <|> do char '('
+                 x <- try parseList <|> parseDottedList
+                 char ')'
+                 return x
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "scheme" input of
+    Left err    -> "No match: " ++ show err
+    Right val   -> "Found value"
+
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn ("Hello, " ++ args !! 0)
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
+
